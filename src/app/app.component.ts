77baa484bbc8201225e5e1fc18dd2d15341bf5f6
Philipp Schön <philipp.schoen@demirtag.de>
2025-03-15 21:22:52 +0100

    Implemented Sudoku Visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgFor} from '@angular/common';
+import {SudokuValue, ValueType} from './sudoku-value';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'sudoku-generator';
+
+  sudoku: (SudokuValue | undefined)[][] = [];
+  type = ValueType;
+
+  constructor() {
+    for(let i = 0; i < 9; i++) {
+      let row = [];
+      for(let j = 0; j < 9; j++) {
+        const value = Math.ceil(Math.random() * 9);
+        row.push({type: ValueType.Predefined, value: Math.ceil(Math.random() * 9)});
+      }
+      this.sudoku.push(row);
+    }
+
+    for(let i = 0; i < 9; i++) {
+      for(let j = 0; j < 9; j++) {
+        if(Math.random() < 0.5) {
+          this.sudoku[i][j] = undefined;
+        }
+      }
+    }
+  }
 }
---
+++ src/app/sudoku-value.ts
@@ -0,0 +1,10 @@
+export interface SudokuValue {
+  type: ValueType;
+  value: number;
+}
+
+export enum ValueType {
+  Predefined,
+  User,
+  Note
+}
