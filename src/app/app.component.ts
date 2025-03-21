fdd032c968620281004dc575264bdc0acc704099
Philipp Schön <philipp.schoen@demirtag.de>
2025-03-21 21:37:57 +0100

    Implemented animation for incorrect solutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import {Component, HostListener, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {NgFor, NgIf} from '@angular/common';
+import {NgClass, NgFor, NgIf} from '@angular/common';
 import {SudokuValue, ValueType} from './sudoku-value';
+import {concat, concatMap, delay, interval, map, of, Subject, take} from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [NgFor, NgIf],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,6 +15,10 @@ export class AppComponent {
   type = ValueType;
 
   markedCell: {x: number, y: number} | undefined;
+
+  incorrectSolution = false;
+
+  currentRotation = 0;
 
   constructor() {
     this.generateSudoku();
@@ -50,8 +55,38 @@ export class AppComponent {
   }
 
   verifySolution() {
+    if(true) {
+      this.animateIncorrectSolution();
+      return false;
+    }
     return true;
   }
+
+animateIncorrectSolution() {
+  this.incorrectSolution = true;
+  const animationSteps = concat(
+    of(0),
+    of(5),
+    of(10),
+    of(5),
+    of(0),
+    of(-5),
+    of(-10),
+    of(-5),
+    of(0)
+  );
+
+  animationSteps.pipe(
+    concatMap(rotation => of(rotation).pipe(delay(35))),
+    map(rotation => {
+      this.currentRotation = rotation;
+      return rotation;
+    }),
+    take(9)
+  ).subscribe({
+    complete: () => this.incorrectSolution = false
+  });
+}
 
   markCell(x: number, y: number) {
     this.markedCell = {x, y};
